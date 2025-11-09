aa7b49b700e9315046adb5bdd728ced6ecfd6e97
João <joaoppm3@al.insper.edu.br>
2025-11-09 18:39:40 -0300

    Tamanho e cores

---
+++ config.py
@@ -0,0 +1,18 @@
+from os import path
+
+WIDTH = 1200
+HEIGHT = 600
+FPS = 60
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+INIT = 0
+INFO = 1
+GAME = 2
+GAME_OVER = 3
+QUIT = 4
