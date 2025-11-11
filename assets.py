b40002838605c78bdcbe9427ae767a6b492b7f04
Teo <teopl@al.insper.edu.br>
2025-11-11 00:56:08 -0300

    Cenário e player

---
+++ assets.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+from config import *
+
+STREET_IMG = 'street'
+GRASS_IMG = 'grass'
+PLAYER_IMG = 'player'
+
+def load_assets():
+    assets = {}
+
+    assets[GRASS_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'grama.png')).convert_alpha()
+    assets[STREET_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'rua.png')).convert_alpha()
+    assets[PLAYER_IMG] = pygame.image.load(os.path.join(IMG_DIR, 'player.png')).convert_alpha()
+
+    return assets
